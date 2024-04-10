6e4d7e20d1409cde198d9b4e7ebac0a8f5c3b647
marcelorafael <marcelorafael23@hotmail.com>
2024-04-09 21:32:02 -0300

    feat: SignUp page created

---
+++ src/pages/Dashboards/index.tsx
@@ -0,0 +1,7 @@
+export default function Dashboards() {
+ return (
+   <div>
+    <h1>Página Dashboards</h1>
+   </div>
+ );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import Dashboards from "../pages/Dashboards";
 
 export default function RoutesApp() {
- return (
-   <Routes>
-    <Route path="/" element={<SignIn />} />
-    <Route path="/register" element={<SignUp />} />
-   </Routes>
- );
+  return (
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/register" element={<SignUp />} />
+      <Route path="/dashboards" element={<Dashboards />} />
+    </Routes>
+  );
 } 
